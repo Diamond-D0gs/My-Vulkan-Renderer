8226a75ce1b4e1610fbd3687c49478adea9258e0
Diamond-D0gs <32776854+Diamond-D0gs@users.noreply.github.com>
2024-01-29 01:09:56 -0500

    CMake fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+ 
+#include <volk.h>
+#include <VkBootstrap.h>
+
+#define REQUIRED_VK_VERSION VK_MAKE_API_VERSION(0, 1, 2, 0)
+#define VK_FAILED(vkResult) (vkResult < 0)
 
 int main(void) {
-    std::cout << "Hello World!";
+    if (VK_FAILED(volkInitialize()))
+        return -1;
+
+    if (volkGetInstanceVersion() < REQUIRED_VK_VERSION)
+        return -1;
+
+    return 0;
 }
